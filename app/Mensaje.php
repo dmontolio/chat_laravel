ede85b92ac50e256e38c8c45dfe1c9ca7a2fe4e7
Daniel Montolio <dmontolio@gmail.com>
2017-05-17 18:16:33 -0400

    Installed packages barryvdh/laravel-debugbar and laravelcollective/html Created class and table Mensajes

---
+++ app/Mensaje.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMensajesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensajes', function (Blueprint $table) {
+            $table->increments('id');
+
+            // Nombre del usuario
+            $table->string('usuario');
+
+            // Texto que el usuario ingreso
+            $table->string('texto');
+
+            // Fecha en que lo hizo
+            $table->timestamp('fecha');
+
+            // created and updated at ..
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mensajes');
+    }
+}
